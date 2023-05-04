5d36e418f5149b3c4f03cd733e3c19863f30b605
Marco Yu Cordero <yu.marco7@gmail.com>
2023-05-04 01:22:08 -0400

    Cambio a firebase y añadiendo funcionalidades de reportes

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const {
-  MongoClient,
-  ServerApiVersion
-} = require('mongodb');
 const app = express();
 
 console.log('Starting server...');
@@ -11,67 +7,10 @@ console.log('Starting server...');
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://marcoyu:catalist@catalist.mkcnmul.mongodb.net/?retryWrites=true&w=majority';
+console.log('Setting up routes...');
 
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
-
-async function run() {
-  try {
-    await client.connect();
-    const db = client.db('Catalist');
-    console.log('Connected to MongoDB');
-
-    console.log('Setting up routes...');
-
-    app.post('/clientes', async (req, res) => {
-      const cliente = req.body;
-      try {
-        const collection = db.collection('clientes');
-        await collection.insertOne(cliente);
-        res.status(201).json({
-          message: 'Cliente creado con éxito'
-        });
-      } catch (error) {
-        console.error('Error al crear cliente', error);
-        res.status(500).json({
-          message: 'Error al crear cliente'
-        });
-      }
-    });
-
-    app.get('/reporte_clientes', async (req, res) => {
-      try {
-        const collection = client.db("Catalist").collection('clientes');
-        const data = await collection.find({
-          type: 'cliente'
-        }).toArray();
-        res.json(data);
-      } catch (error) {
-        console.error('Error al obtener el reporte de clientes', error);
-        res.status(500).json({
-          message: 'Error al obtener el reporte de clientes'
-        });
-      }
-    });
-
-    app.use(express.static('public'));
-
-    app.listen(3000, () => {
-      console.log('Server started on port 3000');
-    });
-  } catch (error) {
-    console.error('Error connecting to MongoDB', error);
-  } finally {
-    // We don't close the client here, because it will close the connection to MongoDB
-  }
-}
-
-run().catch(console.dir);
